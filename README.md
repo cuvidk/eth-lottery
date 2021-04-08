574518014a8b7469690089cd85516347247f226a
Corvin Tiperciuc <cuvidk@gmail.com>
2021-04-08 12:36:19 +0300

    Implemented the pick a winner button

---
+++ README.md
@@ -0,0 +1,15 @@
+# eth-lottery
+
+An ether lottery dapp. Contains files used to create, compile and deploy the lottery smart contract. It also includes a React app used to easily interact with the contract.
+
+## TODO:
+
+- [ ] work on better error handling when interacting with the bchain / contract
+- [ ] add some text area where you display error messages
+- [ ] update lottery info after joining a contract
+- [ ] update lottery info after picking a winner
+- [ ] update lottery info periodically
+- [ ] tune the deploy script to write the abi and the contract address in a .env file
+- [ ] create the lottery contract instance using the abi and address provided in the .env
+- [ ] deploy a new contract that remembers the last prize pool
+- [ ] design a better ui for mobile users
--- src/index.js
+++ src/index.js
@@ -7,33 +7,32 @@ import web3 from "./web3";
 class App extends React.Component {
   state = {
     joined: false,
-    joining: false,
+    loading: false,
+    account: "",
+    isOwner: false,
   };
 
   async componentDidMount() {
+    const owner = await lottery.methods.owner().call();
     const accounts = await web3.eth.getAccounts();
     const players = await lottery.methods.getPlayers().call();
-    players.forEach((player) => {
-      if (accounts[0] === player) {
-        this.setState({ joined: true });
-      }
+
+    this.setState({
+      joined: players.some((player) => player == accounts[0]),
+      isOwner: owner === accounts[0],
+      account: accounts[0],
     });
   }
 
   handleJoin = async () => {
-    const accounts = await web3.eth.getAccounts();
-    if (!accounts.length) {
-      return;
-    }
-
     const result = await lottery.methods
       .join()
       .send({
-        from: accounts[0],
+        from: this.state.account,
         value: web3.utils.toWei("0.01", "ether"),
       })
       .on("sending", () => {
-        this.setState({ joining: true });
+        this.setState({ loading: true });
       })
       .catch((err) => {
         //TODO: render me somewhere
@@ -41,8 +40,25 @@ class App extends React.Component {
       });
 
     result
-      ? this.setState({ joining: false, joined: true })
-      : this.setState({ joining: false });
+      ? this.setState({ loading: false, joined: true })
+      : this.setState({ loading: false });
+  };
+
+  handlePickWinner = async () => {
+    await lottery.methods
+      .pickWinner()
+      .send({
+        from: this.state.account,
+        gas: "1000000",
+      })
+      .on("sending", () => {
+        this.setState({ loading: true });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    this.setState({ loading: false });
   };
 
   render() {
@@ -55,22 +71,35 @@ class App extends React.Component {
         <div className="d-grid col-4 mx-auto">
           <button
             type="button"
-            className="btn-lg btn-dark"
+            className="btn-lg btn-dark m-1"
             onClick={this.handleJoin}
             disabled={this.state.joined}
           >
             <span
               className={`spinner-border spinner-border-sm ${
-                this.state.joining ? "" : "visually-hidden"
+                this.state.loading ? "" : "visually-hidden"
               }`}
               role="status"
               aria-hidden="true"
             ></span>
-            {this.state.joined
-              ? "Already joined"
-              : this.state.joining
-              ? ""
-              : "Join"}
+            {this.state.loading ? "" : this.state.joined ? "Joined" : "Join"}
+          </button>
+          <button
+            type="button"
+            className={`btn-lg btn-dark m-1 ${
+              this.state.isOwner ? "" : "visually-hidden"
+            }`}
+            onClick={this.handlePickWinner}
+            disabled={!this.state.isOwner}
+          >
+            <span
+              className={`spinner-border spinner-border-sm ${
+                this.state.loading ? "" : "visually-hidden"
+              }`}
+              role="status"
+              aria-hidden="true"
+            ></span>
+            {this.state.loading ? "" : "Pick a winner"}
           </button>
         </div>
       </React.StrictMode>
