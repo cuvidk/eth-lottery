f2ed8844128cc57bd8aa93f0877f32e822b62960
Corvin Tiperciuc <cuvidk@gmail.com>
2021-04-08 12:58:56 +0300

    Tuned the deploy script to write the abi and the contract address in a .env file

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ An ether lottery dapp. Contains files used to create, compile and deploy the lot
 - [ ] update lottery info after joining a contract
 - [ ] update lottery info after picking a winner
 - [ ] update lottery info periodically
-- [ ] tune the deploy script to write the abi and the contract address in a .env file
+- [x] tune the deploy script to write the abi and the contract address in a .env file
 - [ ] create the lottery contract instance using the abi and address provided in the .env
 - [ ] deploy a new contract that remembers the last prize pool
 - [ ] design a better ui for mobile users
--- src/contracts/deploy.js
+++ src/contracts/deploy.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const Web3 = require("web3");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const lottery = require("./compile");
-const ganache = require("ganache-cli");
+const path = require("path");
+const fs = require("fs");
 
 const provider = new HDWalletProvider({
   mnemonic: process.env.WALLET_MNEMONIC,
@@ -13,7 +14,7 @@ const web3 = new Web3(provider);
 (async () => {
   const accounts = await web3.eth.getAccounts();
   const sender = accounts[0];
-  const contractInstance = await new web3.eth.Contract(lottery.abi)
+  const result = await new web3.eth.Contract(lottery.abi)
     .deploy({
       data: "0x" + lottery.evm.bytecode.object,
     })
@@ -22,18 +23,23 @@ const web3 = new Web3(provider);
       gas: "1000000",
     })
     .on("sending", () => {
-      console.log("Deploying contract");
-      console.log("-----------------");
-      console.log(JSON.stringify(lottery.abi));
+      console.log("Deploying contract...");
     })
-    .on("error", (err) => {
+    .catch((err) => {
       console.log(err);
-      provider.engine.stop();
-      return;
     });
-  console.log("Contract deployed");
-  console.log("-----------------");
-  console.log(`Contract addr: ${contractInstance.options.address}`);
-  console.log(`From: ${sender}`);
+
+  if (!result) return;
+
+  const outContent =
+    "\nREACT_APP_CONTRACT_ABI = " +
+    JSON.stringify(lottery.abi) +
+    "\nREACT_APP_CONTRACT_ADDR = " +
+    result.options.address;
+  const outPath = path.resolve(__dirname, "..", "..", ".env");
+
+  fs.writeFileSync(outPath, outContent, { encoding: "utf8", flag: "a" });
+  console.log(`Wrote deployment data to ${outPath}`);
+
   provider.engine.stop();
 })();
