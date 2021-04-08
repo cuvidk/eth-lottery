25f06844876714e46096d8db6e3403d54984b56f
Corvin Tiperciuc <cuvidk@gmail.com>
2021-04-08 10:30:42 +0300

    The join process is now more dynamic, containing a spinner and an error handling mechanism; moved lottery data fetching in LotteryInfoList

--- src/components/lottery_info_list.js
+++ src/components/lottery_info_list.js
@@ -1,27 +1,71 @@
 import React from "react";
 import LotteryInfoListItem from "./lottery_info_list_item";
+import web3 from "../web3";
+import lottery from "../lottery";
 
-const LotteryInfoList = ({
-  lastWinner,
-  lastPrizePool,
-  registeredPlayers,
-  prizePool,
-  contractAddress,
-  contractOwner,
-}) => {
-  return (
-    <div className="container-fluid">
-      <LotteryInfoListItem
-        name="Registered players"
-        value={registeredPlayers}
-      />
-      <LotteryInfoListItem name="Prize pool" value={prizePool} />
-      <LotteryInfoListItem name="Last prize pool" value={lastPrizePool} />
-      <LotteryInfoListItem name="Last winner" value={lastWinner} />
-      <LotteryInfoListItem name="Contract addr" value={contractAddress} />
-      <LotteryInfoListItem name="Contract owner" value={contractOwner} />
-    </div>
-  );
-};
+class LotteryInfoList extends React.Component {
+  state = {
+    lastWinner: "",
+    lastPrizePool: "",
+    registeredPlayers: "",
+    prizePool: "",
+    contractAddress: "",
+    contractOwner: "",
+    fetching: false,
+  };
+
+  componentDidMount() {
+    this.fetchLotteryInfo();
+  }
+
+  async fetchLotteryInfo() {
+    const lastWinner = await lottery.methods.winner().call();
+    const lastPrizePool = "0";
+    const registeredPlayers = (await lottery.methods.getPlayers().call())
+      .length;
+    const prizePool = await web3.eth.getBalance(lottery.options.address);
+    const contractAddress = lottery.options.address;
+    const contractOwner = await lottery.methods.owner().call();
+
+    this.setState({
+      lastWinner,
+      lastPrizePool,
+      registeredPlayers,
+      prizePool,
+      contractAddress,
+      contractOwner,
+    });
+  }
+
+  render() {
+    const prize =
+      this.state.prizePool === ""
+        ? ""
+        : web3.utils.fromWei(this.state.prizePool, "ether") + " ethers";
+    const lastPrize =
+      this.state.lastPrizePool === ""
+        ? ""
+        : web3.utils.fromWei(this.state.lastPrizePool, "ether") + " ethers";
+    return (
+      <div className="container-fluid">
+        <LotteryInfoListItem
+          name="Registered players"
+          value={this.state.registeredPlayers}
+        />
+        <LotteryInfoListItem name="Prize pool" value={prize} />
+        <LotteryInfoListItem name="Last prize pool" value={lastPrize} />
+        <LotteryInfoListItem name="Last winner" value={this.state.lastWinner} />
+        <LotteryInfoListItem
+          name="Contract addr"
+          value={this.state.contractAddress}
+        />
+        <LotteryInfoListItem
+          name="Contract owner"
+          value={this.state.contractOwner}
+        />
+      </div>
+    );
+  }
+}
 
 export default LotteryInfoList;
--- src/index.js
+++ src/index.js
@@ -6,36 +6,44 @@ import web3 from "./web3";
 
 class App extends React.Component {
   state = {
-    lastWinner: "...",
-    lastPrizePool: "...",
-    registeredPlayers: "...",
-    prizePool: "...",
-    contractAddress: "...",
-    contractOwner: "...",
+    joined: false,
+    joining: false,
   };
 
-  componentDidMount() {
-    this.updateLotteryInfo();
-  }
-
-  async updateLotteryInfo() {
-    const lastWinner = await lottery.methods.winner().call();
-    const lastPrizePool = 0;
-    const registeredPlayers = (await lottery.methods.getPlayers().call())
-      .length;
-    const prizePool = await web3.eth.getBalance(lottery.options.address);
-    const contractAddress = lottery.options.address;
-    const contractOwner = await lottery.methods.owner().call();
-
-    this.setState({
-      lastWinner,
-      lastPrizePool,
-      registeredPlayers,
-      prizePool,
-      contractAddress,
-      contractOwner,
+  async componentDidMount() {
+    const accounts = await web3.eth.getAccounts();
+    const players = await lottery.methods.getPlayers().call();
+    players.forEach((player) => {
+      if (accounts[0] === player) {
+        this.setState({ joined: true });
+      }
     });
   }
+
+  handleJoin = async () => {
+    const accounts = await web3.eth.getAccounts();
+    if (!accounts.length) {
+      return;
+    }
+
+    const result = await lottery.methods
+      .join()
+      .send({
+        from: accounts[0],
+        value: web3.utils.toWei("0.01", "ether"),
+      })
+      .on("sending", () => {
+        this.setState({ joining: true });
+      })
+      .catch((err) => {
+        //TODO: render me somewhere
+        console.log("error occured:" + err);
+      });
+
+    result
+      ? this.setState({ joining: false, joined: true })
+      : this.setState({ joining: false });
+  };
 
   render() {
     return (
@@ -43,17 +51,26 @@ class App extends React.Component {
         <div className="jumbotron bg-dark text-white text-center p-3">
           <h1>Ether lottery, wanna join?</h1>
         </div>
-        <LotteryInfoList
-          lastWinner={this.state.lastWinner}
-          lastPrizePool={this.state.lastPrizePool}
-          registeredPlayers={this.state.registeredPlayers}
-          prizePool={this.state.prizePool}
-          contractAddress={this.state.contractAddress}
-          contractOwner={this.state.contractOwner}
-        />
+        <LotteryInfoList />
         <div className="d-grid col-4 mx-auto">
-          <button type="button" className="btn-lg btn-dark">
-            Join
+          <button
+            type="button"
+            className="btn-lg btn-dark"
+            onClick={this.handleJoin}
+            disabled={this.state.joined}
+          >
+            <span
+              className={`spinner-border spinner-border-sm ${
+                this.state.joining ? "" : "visually-hidden"
+              }`}
+              role="status"
+              aria-hidden="true"
+            ></span>
+            {this.state.joined
+              ? "Already joined"
+              : this.state.joining
+              ? ""
+              : "Join"}
           </button>
         </div>
       </React.StrictMode>
